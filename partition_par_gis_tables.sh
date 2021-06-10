0c84c8bb53c56ed280bf4bebe65ca93357ac5bde
Bryce Nordgren <bryce.l.nordgren@usda.gov>
2021-06-10 09:47:20 -0600

    need finer grained partitions as simulation progresses.

--- partition_par_gis_tables.sh
+++ partition_par_gis_tables.sh
@@ -8,5 +8,7 @@ psql -h $host -U postgres -c "DROP TABLE ${table}" ei
 psql -h $host -U postgres -c "ALTER TABLE temp RENAME TO ${table}" ei
 psql -h $host -U postgres -c "CREATE TABLE ${table}_p1 PARTITION OF ${table} FOR VALUES FROM ('2017-08-25') TO ('2017-08-26')" ei
 psql -h $host -U postgres -c "CREATE TABLE ${table}_p2 PARTITION OF ${table} FOR VALUES FROM ('2017-08-26') TO ('2017-08-27')" ei
-psql -h $host -U postgres -c "CREATE TABLE ${table}_p3 PARTITION OF ${table} FOR VALUES FROM ('2017-08-27') TO ('2017-08-28')" ei
-psql -h $host -U postgres -c "CREATE TABLE ${table}_p4 PARTITION OF ${table} FOR VALUES FROM ('2017-08-28') TO ('2017-08-29')" ei
+psql -h $host -U postgres -c "CREATE TABLE ${table}_p3 PARTITION OF ${table} FOR VALUES FROM ('2017-08-27') TO ('2017-08-27 12:00')" ei
+psql -h $host -U postgres -c "CREATE TABLE ${table}_p4 PARTITION OF ${table} FOR VALUES FROM ('2017-08-27 12:00') TO ('2017-08-28')" ei
+psql -h $host -U postgres -c "CREATE TABLE ${table}_p5 PARTITION OF ${table} FOR VALUES FROM ('2017-08-28') TO ('2017-08-28 12:00')" ei
+psql -h $host -U postgres -c "CREATE TABLE ${table}_p6 PARTITION OF ${table} FOR VALUES FROM ('2017-08-28 12:00') TO ('2017-08-29')" ei
