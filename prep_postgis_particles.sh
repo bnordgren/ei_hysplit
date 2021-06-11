422236ed124b480c0fb2fa6dd2e9291e3eb016ef
Bryce Nordgren <bryce.l.nordgren@usda.gov>
2021-06-11 16:00:12 -0600

    ogr2ogr, as I am currently using it, produced a geom column named _ogr_geometry_

--- prep_postgis_particles.sh
+++ prep_postgis_particles.sh
@@ -6,6 +6,6 @@ host=192.168.59.39
 #psql -h $host -U postgres -c "DELETE from $table WHERE wkb_geometry = ST_GeomFromText('POINT(0 0)', 4326)" ei
 #psql -h $host -U postgres -c "ALTER TABLE $table ADD COLUMN ts TIMESTAMP" ei
 #psql -h $host -U postgres -c "UPDATE $table SET ts=(substring(time,2,11)||'00')::timestamp" ei
-psql -h $host -U postgres -c "CREATE INDEX on ${table} USING GIST (wkb_geometry)" ei
+psql -h $host -U postgres -c "CREATE INDEX on ${table} USING GIST (_ogr_geometry_)" ei
 psql -h $host -U postgres -c "CREATE INDEX on ${table}(time)" ei
 psql -h $host -U postgres -c "CREATE INDEX on ${table}(ptyp)" ei
